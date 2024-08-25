bf2a227b4d5350d50687adbbbfeaa1ede7cffd21
Daniel Alanis <danielalanis@MacBook-Pro-de-Daniel.local>
2024-08-25 18:46:48 -0300

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/add_tutorial/add_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddTutorialScreen extends StatelessWidget {
+  static const String name = 'add_tutorial_screen';
+  const AddTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
